5bc1801aac390711d138844dc2e0f101ea052ab3
Leonardo da Vinci <leonardodavinci2049@gmail.com>
2025-06-10 10:20:07 -0300

    refactor: ajustar importações e estrutura do componente ActionPage; adicionar funções para revalidação de caminhos e tags

--- src/app/acoes/page.tsx
+++ src/app/acoes/page.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React from "react";
 const API_URL = process.env.API_URL;
 
 type ApiLuaResponse = {
@@ -8,43 +8,51 @@ type ApiLuaResponse = {
 };
 
 const ActionPage = async () => {
+  if (!API_URL) {
+    throw new Error("API_URL is not defined in the environment variables");
+  }
 
- const response = await fetch(`${API_URL}/acoes/lua`, {
-    cache: "no-store", // Ensures the data is always fresh
-    //cache: "force-cache", // Use cache for performance, but can be changed based on requirements
-    //  next: { revalidate: 60 }, // Revalidate every 60 seconds
-    headers: {
-      "Content-Type": "application/json",
-      Accept: "application/json",
-    },
-  });
+  try {
+    const response = await fetch(`${API_URL}/acoes/lua`, {
+      // cache: "no-store", // Ensures the data is always fresh
+      //cache: "force-cache", // Use cache for performance, but can be changed based on requirements
+      next: { revalidate: 60 }, // Revalidate every 60 seconds
+      headers: {
+        "Content-Type": "application/json",
+        Accept: "application/json",
+      },
+    });
 
     const data = (await response.json()) as ApiLuaResponse;
-  const DateNow = new Date().toLocaleString("pt-BR");
+    const DateNow = new Date().toLocaleString("pt-BR");
 
-
-  return (
-    <div>
-      <h1>Action Lua</h1>
-      <ul>
-        <li>
-          <strong>Nome:</strong> {data.nome} <br />{" "}
-        </li>
-        <li>
-          <strong>Preço:</strong> {data.preco} <br />
-        </li>
-        <li>
-          <strong>Atualizado:</strong> {data.atualizada} <br />
-        </li>
-        <li>
-          <strong>Data:</strong> {DateNow} <br />
-        </li>
-      </ul>
-      <div className="flex justify-center items-center mt-4">
-       {/* <UpdateUrlButton /> */}
+    return (
+      <div>
+        <h1>Action Lua</h1>
+        <ul>
+          <li>
+            <strong>Nome:</strong> {data.nome} <br />{" "}
+          </li>
+          <li>
+            <strong>Preço:</strong> {data.preco} <br />
+          </li>
+          <li>
+            <strong>Atualizado:</strong> {data.atualizada} <br />
+          </li>
+          <li>
+            <strong>Data:</strong> {DateNow} <br />
+          </li>
+        </ul>
+        <div className="flex justify-center items-center mt-4">
+          {/* <UpdateUrlButton /> */}
+        </div>
       </div>
-    </div>
-  )
-}
+    );
+  } catch (error) {
+    console.error("Fetch error:", error);
+    // Retorne uma UI de fallback
+    return <div>Erro ao carregar dados</div>;
+  }
+};
 
-export default ActionPage
+export default ActionPage;
